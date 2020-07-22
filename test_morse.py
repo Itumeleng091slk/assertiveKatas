bc375430f21d9597db9cbe71d789835b3cd08edc
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-07-22 10:51:55 +0200

    Update

--- test_morse.py
+++ test_morse.py
@@ -2,10 +2,10 @@ from morsecode import *
 
 def test_decode_morse():
     message1 = "Hi-There"
-    result = decode_morse(message1.upper())
+    result = decode_to_morse(message1.upper())
     assert len(message1) != 0 , "Does not have the same number of characters in the input & output"
 
 def test_encode_to_morse():
-    message1 = "Hi-There"
-    result = encode_to_morse(message1.upper())
-    assert len(message1) != 0 , "Does not have the same number of characters in the input & output"
+    message2 = ".... .. -....- - .... . .-. ." 
+    result = encode_to_morse(message2.upper())
+    assert len(message2) != 0 , "Does not have the same number of characters in the input & output"
